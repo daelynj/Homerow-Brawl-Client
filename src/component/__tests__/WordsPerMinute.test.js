9ca2690af271ae26e7e0609af56db4087a2b9724
daelynj <daelyn.jones@juul.com>
2019-06-05 15:36:51 -0700

    Add WordsPerMinute component
    We remove the WPM logic from the Board component and place it in its own
    component so that we can further implement better WPM logic in the
    future such as accuracy and incorrect words. This also cleans up the
    Board component.

---
+++ src/component/__tests__/WordsPerMinute.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WordsPerMinute from '../WordsPerMinute';
+import renderer from 'react-test-renderer';
+
+describe("WordsPerMinute", () => {
+  it("renders the expected words per minute", () => {
+    const tree = renderer.create(
+      <WordsPerMinute
+        textLength = {60}
+        finishTime = {60}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
