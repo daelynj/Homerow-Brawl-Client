9fdfb1cb3076820e1a009cdfa98797cd1aebd09e
daelynj <daelyn.jones@juul.com>
2019-06-05 14:56:52 -0700

    Add test for Text component

---
+++ src/component/textbox/__tests__/Text.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Text from '../Text';
+import renderer from 'react-test-renderer';
+
+describe("Text", () => {
+  it("renders the expected text", () => {
+    const tree = renderer.create(
+      <Text
+        text = {["This", "is", "text"]}
+        textToCompare = {""}
+        input = {""}
+        currentWord = {0}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
