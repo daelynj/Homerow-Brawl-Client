6225d5fccc20b084be570f6b688cce17f464ad76
daelynj <daelyn.jones@juul.com>
2019-06-05 10:53:28 -0700

    Add test for Letter component

---
+++ src/component/textbox/__tests__/Letter.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Letter from '../Letter';
+import renderer from 'react-test-renderer';
+
+describe("Letter", () => {
+  it("renders the expected letter in black", () => {
+    const tree = renderer.create(
+      <Letter
+        color = {"black"}
+        letter = {"k"}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
