1f9d6f4110873d11a4c4e096b87b46d003d452bd
daelynj <daelyn.jones@juul.com>
2019-06-10 15:44:57 -0700

    Add test for Board component

---
+++ src/component/__tests__/Board.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Board from '../Board';
+import renderer from 'react-test-renderer';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe("Board", () => {
+  const setStart = jest.fn();
+  const setCurrentInput = jest.fn();
+  const setCurrentWordIndex = jest.fn();
+  const setFinishTime = jest.fn();
+  const checkWord = jest.fn();
+  const endGame = jest.fn();
+
+  const buildProps = (newProps = {}) => ({
+    words: ["this", "is", "text"],
+    start: false,
+    setStart,
+    currentWordSubstring: "",
+    currentInput: "",
+    setCurrentInput,
+    currentWordIndex: 0,
+    setCurrentWordIndex,
+    endGame,
+    finishTime: 0,
+    setFinishTime,
+    checkWord,
+    ...newProps,
+  });
+
+  describe("the game before it's running", () => {
+    it("renders the expected components", () => {
+      const BoardComponent = renderer.create(<Board {...buildProps()}/>).toJSON();
+      expect(BoardComponent).toMatchSnapshot();
+    });
+  });
+
+  describe("the game once it's running", () => {
+    it("renders the expected components", () => {
+      const BoardComponent = renderer.create(<Board {...buildProps({start: true})}/>).toJSON();
+      expect(BoardComponent).toMatchSnapshot();
+    });
+  });
+  
+  describe("the game when it is finished", () => {
+    const endGame = jest.fn().mockReturnValue(true);
+
+    it("renders the expected components", () => {
+      const BoardComponent = renderer.create(<Board {...buildProps({start: true, endGame: endGame})}/>).toJSON();
+      expect(BoardComponent).toMatchSnapshot();
+    });
+  });
+});
