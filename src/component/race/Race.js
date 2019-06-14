b56e3f0af47154d707f3474b3164ab169abe6fda
daelynj <daelyn.jones@juul.com>
2019-06-14 14:22:49 -0700

    Modify how many words it takes to move the image
    We now use a percentage based formula to determine how many words it
    takes for the image to move. This ensures that the image will move the
    same amount of times no matter the length of text we are typing.

--- src/component/race/Race.js
+++ src/component/race/Race.js
@@ -2,15 +2,11 @@ import React from "react";
 import octane from "./images/octane.png";
 
 function Race(props) {
-  const racePositions = 15;
-  const wordsToAdvance = Math.round(props.wordsLength / racePositions);
-
   return (
     <div>
       <img
         style={{
-          paddingLeft:
-            Math.floor(props.currentWordIndex / wordsToAdvance) + "em"
+          paddingLeft: (props.currentWordIndex / props.wordsLength) * 100 + "%"
         }}
         src={octane}
         alt="Octane"
