655fb8e69ee0ffe78659d87fed8c9d9d7fce3548
daelynj <daelyn.jones@juul.com>
2019-06-17 14:21:35 -0700

    Migrate GameStats component to TypeScript

---
+++ src/component/gamestats/GameStats.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+interface Props {
+  words: String[];
+  incorrectLetters: number;
+  finishTime: number;
+}
+
+export const GameStats = (props: Props) => {
+  const getLettersTyped = () => {
+    return props.words.join("").length;
+  };
+
+  const calculateAccuracy = () => {
+    let letters: number = getLettersTyped();
+    let accuracy: number = ((letters - props.incorrectLetters) / letters) * 100;
+
+    return accuracy > 0 ? accuracy : 0;
+  };
+
+  //https://www.speedtypingonline.com/typing-equations
+  const calculateWPM = () => {
+    let allTypedEntries: number = getLettersTyped();
+    let averageWordLength: number = 5;
+    let timePlayed: number = props.finishTime / 60;
+    let grossWPM: number = allTypedEntries / averageWordLength / timePlayed;
+
+    return Math.round(grossWPM);
+  };
+
+  return (
+    <div>
+      <div>{"words typed: " + props.words.length}</div>
+      <div>{"WPM: " + calculateWPM()}</div>
+      <div>{"time: " + props.finishTime + " seconds"}</div>
+      <div>{"mistakes: " + props.incorrectLetters}</div>
+      <div>{"accuracy: " + calculateAccuracy() + "%"}</div>
+    </div>
+  );
+};
--- src/component/gamestats/__tests__/GameStats.test.tsx
+++ src/component/gamestats/__tests__/GameStats.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import GameStats from "../GameStats";
+import * as React from "react";
+import { GameStats } from "../GameStats";
 import renderer from "react-test-renderer";
 
 describe("GameStats", () => {
