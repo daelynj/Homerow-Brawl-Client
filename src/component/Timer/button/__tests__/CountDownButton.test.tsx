d0f952cc3c7313f4f0ea5509faea934e2402a8e6
daelynj <daelyn.jones@juul.com>
2019-07-15 10:25:23 -0700

    Create and update Timer specs

---
+++ src/component/Timer/button/__tests__/CountDownButton.test.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { CountDownButton } from "../CountDownButton";
+import renderer from "react-test-renderer";
+
+describe("CountDownButton", () => {
+  it("passes", () => {});
+});
