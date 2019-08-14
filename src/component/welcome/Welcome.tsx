9fbbddf210a147e165fbe2ac1b1a3866b86b6e60
daelynj <daelyn.jones@juul.com>
2019-08-14 16:57:11 -0700

    Add CSS for generate lobby button
    Welcome is a bad name for this component I'm realizing now, but put that
    aside for now.

--- src/component/welcome/Welcome.tsx
+++ src/component/welcome/Welcome.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { WelcomeButton } from "./WelcomeButton";
 import { createRoomAPI } from "./api/createRoomAPI";
+import "./css/Welcome.css";
 
 export const Welcome = () => {
   const [roomID, setRoomID] = useState<any>(-1);
@@ -9,7 +10,7 @@ export const Welcome = () => {
   const [error, setError] = useState<any>(false);
 
   return (
-    <>
+    <div className="welcome">
       {roomID < 0 && (
         <WelcomeButton
           handleEvent={createRoomAPI(setRoomID, setIsLoaded, setError)}
@@ -17,6 +18,6 @@ export const Welcome = () => {
       )}
       {!error && isLoaded && window.location.origin + "/" + roomID}
       {error && error.message}
-    </>
+    </div>
   );
 };
