e942ad6914414f958814b579b0e14a2b05220907
daelynj <daelyndbjones@gmail.com>
2019-08-22 15:50:04 -0700

    Add Navbar

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,14 +2,22 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Authentication } from "./Authentication";
 import { Stats } from "./globalstats/Stats";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const App = () => (
   <>
     <Router>
-      <nav>
-        <Link to="/stats">Stats</Link>
-      </nav>
-
+      <Navbar bg="primary" variant="dark">
+        <LinkContainer to="/">
+          <Navbar.Brand>Homerow Brawl</Navbar.Brand>
+        </LinkContainer>
+        <Nav>
+          <LinkContainer to="/stats">
+            <Nav.Link>Stats</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar>
       <Route path="/stats" component={Stats} />
       <Route path="/" exact component={Authentication} />
       <Route path="/:id(\d+)" exact component={Authentication} />
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./component/App";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
