b8ea023ac91ad3cac8d82cb637b5613213b1875a
daelynj <daelyndbjones@gmail.com>
2019-08-22 14:04:32 -0700

    Modify lots of CSS

--- src/component/slack/SlackButton.tsx
+++ src/component/slack/SlackButton.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
 import sign_in_with_slack from "./images/sign_in_with_slack.png";
-import "./css/SlackButton.css";
 
 interface Props {
   handleEvent: (newEvent: any) => void;
 }
 
 export const SlackButton = (props: Props) => (
-  <img
-    className="button"
-    src={sign_in_with_slack}
-    alt="Slack"
-    onClick={props.handleEvent}
-  />
+  <img src={sign_in_with_slack} alt="Slack" onClick={props.handleEvent} />
 );
