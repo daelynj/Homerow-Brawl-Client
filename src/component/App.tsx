928a19315ff700be194f7818ff950b830c0b0552
daelynj <daelyndbjones@gmail.com>
2019-08-28 16:35:43 -0700

    Remove unused definition found by linter

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Authentication } from "./Authentication";
 import { Stats } from "./globalstats/Stats";
 import { Navbar, Nav } from "react-bootstrap";
