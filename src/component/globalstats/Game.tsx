2f7a4206710b8c401de6eedc1cee54e96e099e8a
daelynj <daelyndbjones@gmail.com>
2019-08-28 14:41:05 -0700

    Format date/time in Game history
    Now we will see the Date listed as (for example):
    Tue Aug 27 2019 2:49:17 PM
    
    instead of:
    2019-08-27 20:05:26 UTC

--- src/component/globalstats/Game.tsx
+++ src/component/globalstats/Game.tsx
@@ -5,39 +5,45 @@ interface Props {
   game: any;
 }
 
-export const Game = (props: Props) => (
-  <>
-    <tr>
-      <th>
-        <div>Date</div>
-      </th>
-      <th>
-        <div>Time</div>
-      </th>
-      <th>
-        <div>WPM</div>
-      </th>
-      <th>
-        <div>Accuracy</div>
-      </th>
-      <th>
-        <div>Mistakes</div>
-      </th>
-      <th>
-        <div>Words</div>
-      </th>
-      <th>
-        <div>Letters</div>
-      </th>
-    </tr>
-    <tr>
-      <td>{props.game.created_at}</td>
-      <td>{props.game.time + " seconds"}</td>
-      <td>{props.game.wpm}</td>
-      <td>{props.game.accuracy + "%"}</td>
-      <td>{props.game.mistakes}</td>
-      <td>{props.game.words_typed}</td>
-      <td>{props.game.letters_typed}</td>
-    </tr>
-  </>
-);
+export const Game = (props: Props) => {
+  const buildDate = () => {
+    let timestamp = new Date(Date.parse(props.game.created_at));
+    return timestamp.toDateString() + "\n" + timestamp.toLocaleTimeString();
+  };
+  return (
+    <tbody>
+      <tr>
+        <th>
+          <div>Date</div>
+        </th>
+        <th>
+          <div>Time</div>
+        </th>
+        <th>
+          <div>WPM</div>
+        </th>
+        <th>
+          <div>Accuracy</div>
+        </th>
+        <th>
+          <div>Mistakes</div>
+        </th>
+        <th>
+          <div>Words</div>
+        </th>
+        <th>
+          <div>Letters</div>
+        </th>
+      </tr>
+      <tr>
+        <td>{buildDate()}</td>
+        <td>{props.game.time + " seconds"}</td>
+        <td>{props.game.wpm}</td>
+        <td>{props.game.accuracy + "%"}</td>
+        <td>{props.game.mistakes}</td>
+        <td>{props.game.words_typed}</td>
+        <td>{props.game.letters_typed}</td>
+      </tr>
+    </tbody>
+  );
+};
