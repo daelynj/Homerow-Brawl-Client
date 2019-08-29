23c7197df1a6dc191f75f24e98b13c5eb556a266
daelynj <daelyndbjones@gmail.com>
2019-08-29 15:36:37 -0700

    Follow lifecycle pattern for API calls
    Making this API call is something we want to do as soon as we can. By
    placing it in a lifecycle block we make this API call after the first
    render any time you navigate to this page if you aren't already
    authenticated. This call can only be made after you have signed in
    with Slack as seen by the conditional call.
    
    This ensures that this call is only happening once every time you
    access the page, the first time it gets turned away, the second time
    (after OAuth with Slack) the call goes through.

--- src/component/slack/api/handleSlackOAuth.ts
+++ src/component/slack/api/handleSlackOAuth.ts
@@ -7,25 +7,21 @@ export function handleURL(setAuthenticated: any) {
   var error = search_params.get("error");
 
   if (code !== null && state === "goodtimes" && error !== "access_denied") {
-    getSlackOAuth(code, setAuthenticated);
+    var entryURL = "http://" + window.location.host + window.location.pathname;
+    var data: any = { code: code, redirectURI: entryURL };
+
+    fetch("http://localhost:3000/api/slack/oauth", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(result => {
+        sessionStorage.setItem("uuid", result.uuid);
+        sessionStorage.setItem("authenticated", result.authenticated);
+
+        setAuthenticated(result.authenticated);
+      });
   }
 }
-
-const getSlackOAuth = (code: any, setAuthenticated: any) => {
-  var entryURL = "http://" + window.location.host + window.location.pathname;
-  var data: any = { code: code, redirectURI: entryURL };
-
-  fetch("http://localhost:3000/api/slack/oauth", {
-    method: "POST",
-    mode: "cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  })
-    .then(response => response.json())
-    .then(result => {
-      sessionStorage.setItem("uuid", result.uuid);
-      sessionStorage.setItem("authenticated", result.authenticated);
-
-      setAuthenticated(result.authenticated);
-    });
-};
